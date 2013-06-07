3d128ea91afc48d7cd8fe0e06f743954503dd745
Reedy <reedy@wikimedia.org>
2013-06-07 14:39:49 +0100

    Initial version of script to automatically upload changelogs to mw.org
    Uses botclasses.php
    
    https://toolserver.org/~chris/highlight.php?d=chris%2Fclasses%2F&f=botclasses.php
    
    Change-Id: Ib50c47b4229c8f7680516e307db180a076f725ba

---
+++ make-deploy-notes/uploadChangelog.php
@@ -0,0 +1,84 @@
+/**
+ * Copyright © 2013 Sam Reed
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @file
+ */
+
+<?php
+
+if ( count( $argv ) !== 2 ) {
+	print "usage: $argv[0] wmf/1.22wmf6\n";
+	exit(1);
+}
+
+$version = $argv[1];
+$previousVersion = getPreviousVersion( $version );
+
+if ( $previousVersion === null ) {
+	print "usage: $argv[0] wmf/1.22wmf6\n";
+	exit(1);
+}
+
+$escVer = escapeshellarg( $version );
+$escPrevVer = escapeshellarg( $previousVersion );
+
+$output = shell_exec( "php -f ./make-deploy-notes {$escPrevVer} {$escVer}" );
+
+require_once( 'botclasses.php' );
+$wiki = new wikipedia( 'https://www.mediawiki.org/w/api.php' );
+
+$wiki->login( '', '' );
+
+list( $major, $minor ) = getMajorMinor( $version );
+$wiki->edit( "MediaWiki 1.{$major}/wmf{$minor}/Changelog", $output, "Update changelog for $version", false, false );
+
+print "Changelog updated\n";
+
+/**
+ * @param $input string
+ * @return string|null
+ */
+function getPreviousVersion( $input ) {
+	$majorMinor = getMajorMinor( $input );
+	if ( $majorMinor === null ) {
+		return null;
+	}
+	list( $major, $minor ) = $majorMinor;
+	if ( $minor === 1 ) {
+		$major--;
+		$minor = 12;
+	} else {
+		$minor--;
+	}
+	return "wmf/1.{$major}wmf{$minor}";
+}
+
+/**
+ * @param $input string
+ * @return array|null
+ */
+function getMajorMinor( $input ) {
+	$matches = array();
+	if ( preg_match( "/^wmf\/1\.(\d{2})wmf(\d{1,2})/", $input, $matches ) ) {
+		// var_dump( $matches );
+		$major = intval( $matches[1] );
+		$minor = intval( $matches[2] );
+		return array( $major, $minor );
+	}
+	return null;
+}
