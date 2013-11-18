65a1a5f4524623cde5407a1abfa74b4938ccb601
Antoine Musso <hashar@free.fr>
2013-11-18 12:55:47 +0100

    make-release: unit tests for MwVersion
    Basic unit tests for the make-release MwVersion class using python
    unittest. Will let us refactor MwVersion to change 'branch' to 'tags'
    and populate the real branch name (aka: REL#_## or master).
    
    $ python -m unittest discover --verbose
    test_incomplete_version (tests.test_mwversion.TestMwVersion) ... ok
    test_major_version (tests.test_mwversion.TestMwVersion) ... ok
    test_master (tests.test_mwversion.TestMwVersion) ... ok
    test_minor_version (tests.test_mwversion.TestMwVersion) ... ok
    test_release_candidate (tests.test_mwversion.TestMwVersion) ... ok
    test_release_candidate_bumps (tests.test_mwversion.TestMwVersion) ... ok
    
    ----------------------------------------------------------------------
    Ran 6 tests in 0.002s
    
    OK
    $
    
    The runtest.sh is a helper to let people easily run the tests without
    having to figure out the exact command.
    
    bug: 57088
    Change-Id: I25f5a9d28e4e1c0bb73ba3603f741d8be2c05f0b

---
+++ make-release/tests/test_mwversion.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+#
+# Copyright 2013, Antoine "hashar" Musso
+# Copyright 2013, Wikimedia Foundation Inc.
+
+
+import unittest
+makerelease = __import__('make-release')
+
+
+class FakeVersion(makerelease.MwVersion):
+
+    def __init__(self, attributes):
+        self.raw = None
+        self.major = None
+        self.branch = None
+        self.prev_version = None
+        self.prev_branch = None
+
+        self.__dict__.update(attributes)
+
+
+class TestMwVersion(unittest.TestCase):
+
+    # Helper
+    def assertMwVersionEqual(self, expected, observed):
+        """Compare two object properties"""
+        return self.assertDictEqual(expected.__dict__, observed.__dict__)
+
+    def test_master(self):
+        observed = makerelease.MwVersion('master')
+        expected = FakeVersion({'raw': 'master'})
+        self.assertMwVersionEqual(expected, observed)
+
+    def test_major_version(self):
+        observed = makerelease.MwVersion('1.22.0')
+        expected = FakeVersion({
+            'raw': '1.22.0',
+            'major': '1.22',
+            'branch': 'tags/1.22.0',
+            })
+        self.assertMwVersionEqual(expected, observed)
+
+    def test_minor_version(self):
+        observed = makerelease.MwVersion('1.22.1')
+        expected = FakeVersion({
+            'raw': '1.22.1',
+            'major': '1.22',
+            'branch': 'tags/1.22.1',
+            'prev_branch': 'tags/1.22.0',
+            'prev_version': '1.22.0',
+            })
+        self.assertMwVersionEqual(expected, observed)
+
+    def test_release_candidate(self):
+        observed = makerelease.MwVersion('1.22.0rc0')
+        expected = FakeVersion({
+            'raw': '1.22.0rc0',
+            'major': '1.22',
+            'branch': 'tags/1.22.0rc0',
+            })
+        self.assertMwVersionEqual(expected, observed)
+
+    def test_release_candidate_bumps(self):
+        observed = makerelease.MwVersion('1.22.0rc0')
+        expected = FakeVersion({
+            'raw': '1.22.0rc0',
+            'major': '1.22',
+            'branch': 'tags/1.22.0rc0',
+            })
+        self.assertMwVersionEqual(expected, observed)
+
+        observed = makerelease.MwVersion('1.22.0rc1')
+        expected = FakeVersion({
+            'raw': '1.22.0rc1',
+            'major': '1.22',
+            'branch': 'tags/1.22.0rc1',
+            'prev_branch': 'tags/1.22.0rc0',
+            'prev_version': '1.22.0rc0',
+            })
+        self.assertMwVersionEqual(expected, observed)
+
+    def test_incomplete_version(self):
+        observed = makerelease.MwVersion('1.22')
+        expected = FakeVersion({'raw': '1.22'})
+        self.assertMwVersionEqual(expected, observed)
