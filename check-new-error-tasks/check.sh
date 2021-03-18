8a8c7baf30057721daf6f3f8f97eb3f99325984d
Brennen Bearnes <bbearnes@wikimedia.org>
2021-03-17 22:45:46 -0600

    check-new-error-tasks: single maniphest.search call, UX improvements
    Faster, automates the downloading of dashboard data, and allows for
    checking other dashboards besides just the hardcoded
    mediawiki-new-errors ID.
    
      - Optionally takes a dashboard ID.
    
      - Uses a single API call instead of a loop around the frozen
        maniphest.info call, and uses jq to format TSV from the
        response.
    
      - Sorts output by task ID.
    
      - Prompts to download JSON, and before overwriting old data if
        present.
    
      - Adds a --help and a README.
    
      - pipefail
    
    Sample output:
    
        $ ./check.sh
        dashboard.json:
        -rw-r--r-- 1 brennen brennen 2.3M Mar 17 23:01 dashboard.json
    
        Overwrite with fresh dashboard data (y/n)? n
        273101  resolved
        277691  open
        277089  open
        277087  resolved
        276386  resolved
        276385  open
        275072  open
        ...
    
    Change-Id: Ia130f4462e54e219faad446b3c619b2cb5adfdea

--- check-new-error-tasks/check.sh
+++ check-new-error-tasks/check.sh
@@ -1,35 +1,98 @@
 #!/bin/bash
 
-# This is a way to check status of current filters on the mediawiki-new-errors
-# logstash dashboard.  It's hacks all the way down.
-#
-# Prerequisites:
-#   jq       - sudo apt install jq
-#   arcanist - https://wikitech.wikimedia.org/wiki/Help:Arcanist
-#
-# Once you have arcanist installed, you'll need to get a token with:
-#
-#   $ arc set-config default https://phabricator.wikimedia.org/
-#   $ arc install-certificate
-#
-# Get the dashboard data with something like:
-#
-# curl --user "[username]" 'https://logstash.wikimedia.org/api/kibana/dashboards/export?dashboard=0a9ecdc0-b6dc-11e8-9d8f-dbc23b470465' -o dashboard_json
-#
-# You'll need to use your LDAP password, which _should_ be safe since this is
-# https.
+set -e -o pipefail
 
-tasks () {
-  jq -r '.objects[0].attributes.kibanaSavedObjectMeta.searchSourceJSON | fromjson | .filter | .[].meta.alias' < dashboard_json \
-    | sed 's/^T\([0-9]\+\).*/\1/' \
-    | grep -E '^[0-9]+$'
+# This is a hardcoded path to the mediawiki-new-errors dashboard.
+# See also:
+#  - https://wikitech.wikimedia.org/wiki/Performance/Runbook/Kibana_monitoring
+#  - https://wikitech.wikimedia.org/wiki/Heterogeneous_deployment/Train_deploys#Places_to_Watch_for_Breakage
+DEFAULT_DASHBOARD_ID='0a9ecdc0-b6dc-11e8-9d8f-dbc23b470465'
+
+usage () {
+  cat <<DOC
+USAGE
+
+  # Check default dashboard - mediawiki-new-errors:
+  ./check.sh
+
+  # Specify a dashboard ID:
+  ./check.sh $DEFAULT_DASHBOARD_ID
+
+  # See this usage info:
+  ./check.sh --help
+
+This is a way to check status of current filters on the
+mediawiki-new-errors logstash dashboard.  It's hacks all the way
+down.
+
+Prerequisites:
+  jq       - sudo apt install jq
+  arcanist - https://wikitech.wikimedia.org/wiki/Help:Arcanist
+
+Once you have arcanist installed, you'll need to get a token with:
+
+  $ arc set-config default https://phabricator.wikimedia.org/
+  $ arc install-certificate
+
+You'll need to use your LDAP password to get dashboard.json, which
+_should_ be safe since this is https.
+
+Note that this will fail if the dashboard has in excess
+of 100 tasks on filters, since it doesn't handle Conduit API
+pagination.  Let's try to have fewer than 100 filters.
+DOC
+
+  exit
 }
 
-# This should almost certainly be a single request with all tasks.  Sleeps
-# in-between calls to be gentle with Phab.
+curl_dashboard () {
+  read -rp "LDAP username for logstash: " username
+  curl --fail --user "$username" "$1" -o "$2"
+}
 
-for task in $(tasks); do
-  status=$(printf '{ "task_id": "%s" }' "$task" | arc call-conduit -- maniphest.info | jq .response.status)
-  echo "$task: $status"
-  sleep 5
-done
+tasks () {
+  jq -r '.objects[0].attributes.kibanaSavedObjectMeta.searchSourceJSON | fromjson | .filter | .[].meta.alias' < "$1" \
+    | sed 's/^T\([0-9]\+\).*/\1/' \
+    | grep -E '^[0-9]+$' \
+    | paste -sd ','
+}
+
+format_response () {
+  jq -r '.response.data | .[] | [.id, .fields.status.value] | @tsv' | sort -u
+}
+
+if [ $# -lt 1 ]; then
+  dashboard_id="$DEFAULT_DASHBOARD_ID"
+elif [ "$1" = '--help' ] || [ "$1" = '-h' ]; then
+  usage
+else
+  dashboard_id="$1"
+fi
+dashboard_path="https://logstash.wikimedia.org/api/kibana/dashboards/export?dashboard=$dashboard_id"
+dashboard_json="$dashboard_id.json"
+
+if ! [ -e "$dashboard_json" ]; then
+  retrieve_dashboard="Y"
+else
+  echo "$dashboard_json"
+  ls -lah "$dashboard_json"
+  echo
+  read -rp "Overwrite with fresh dashboard data (y/n)? " retrieve_dashboard
+fi
+case "$retrieve_dashboard" in
+  [yY][eE][sS]|[yY])
+    curl_dashboard "$dashboard_path" "$dashboard_json"
+    ;;
+esac
+
+# Ask conduit for tasks with matching IDs.  This will break if there
+# are more than 100 tasks with filters, since it doesn't do anything
+# to handle pagination.
+
+printf '{
+  "constraints": {
+    "ids": [
+      %s
+    ]
+  }
+}' "$(tasks "$dashboard_json")" | arc call-conduit -- maniphest.search | format_response
