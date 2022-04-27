edf86f4684729fd506266005a1e80c056d65c495
sbassett <sbassett@wikimedia.org>
2022-04-27 17:58:51 -0500

    Add new permissions for supplemental security tasks
    * For the supplemental security release task which gets created
      as part of a new release process, use the primary security
      subType for the view policy and a subscribers-only policy for
      the edit type.  New logic tested via T307047.
    
    Bug: T307036
    Change-Id: I55fb51741860c1d691edbe70feced5ec2dec19eb

--- make-security-tasks/makesecuritytasks.py
+++ make-security-tasks/makesecuritytasks.py
@@ -51,43 +51,62 @@ Tracking bug for next security release, {0}""".format(version),
     {
         "title": "Obtain CVEs for {0} security releases".format(version),
         "description": "",
+        "viewPolicy": aclSec,
+        "editPolicy": aclSec,
     },
     {
         "title": "Write and send pre-release announcements for MediaWiki {0}".format(version),
         "description": "Previous work: ",
+        "viewPolicy": aclSec,
+        "editPolicy": aclSec,
     },
     {
         "title": "Write and send release announcements for MediaWiki {0}".format(version),
         "description": "Previous work: ",
+        "viewPolicy": aclSec,
+        "editPolicy": aclSec,
     },
     {
         "title": "Tag {0}".format(version),
         "description": "Create and push git tags for {0}".format(version),
+        "viewPolicy": aclSec,
+        "editPolicy": aclSec,
     },
     {
         "title": "Update onwiki news and Module:Version",
         "description": "Update https://www.mediawiki.org/wiki/Template:MediaWiki_News"
-                       " and https://www.mediawiki.org/wiki/Module:Version"
+                       " and https://www.mediawiki.org/wiki/Module:Version",
+        "viewPolicy": aclSec,
+        "editPolicy": aclSec,
     },
     {
         "title": "Update onwiki release notes for {0}".format(version),
         "description":
             "The following mediawiki.org pages need updating from the RELEASE-NOTES files:",
         # TODO: URLS!
+        "viewPolicy": aclSec,
+        "editPolicy": aclSec,
     },
     {
         "title": "Update Wikidata Q83",
-        "description": "Add new releases to https://www.wikidata.org/wiki/Q83"
+        "description": "Add new releases to https://www.wikidata.org/wiki/Q83",
+        "viewPolicy": aclSec,
+        "editPolicy": aclSec,
     },
     {
         "title": "Update HISTORY in master after {0}".format(version),
         "description": ("Point release RELEASE-NOTES from {0} need copying to HISTORY "
                         "in the relevant places in master".format(version)),
+        "viewPolicy": aclSec,
+        "editPolicy": aclSec,
     },
     {
         "title": ("Write and send supplementary release announcement for "
                   "extensions and skins with security patches ({0})".format(version)),
         "description": "Previous work: ",
+        # T307036
+        "viewPolicy": "PHID-PLCY-rw34dnnz7s5d7njlpk5v",
+        "editPolicy": "obj.subscriptions.subscribers",
     },
 ]
 
@@ -97,8 +116,8 @@ for task in subTasks:
     res = phab.maniphest.createtask(
         title=task['title'],
         description=task['description'],
-        viewPolicy=aclSec,
-        editPolicy=aclSec,
+        viewPolicy=task['viewPolicy'],
+        editPolicy=task['editPolicy'],
         projectPHIDs=taskProjects,
     )
     subTaskPHIDS.append(res['phid'])
